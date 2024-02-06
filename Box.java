b80589b5cff552d78caa3ad01c4a09774439c742
EtuIckae <mickael.riego@etu.univ-orleans.fr>
2024-02-06 16:36:40 +0100

    Thing & Box: ajout class Thing et du add de Box

--- Box.java
+++ Box.java
@@ -2,6 +2,6 @@ import java.util.ArrayList;
 class Box{
     ArrayList<String> contents = new ArrayList<String>();
     public void add(String truc) {
-        
+        this.contents.add(truc);
     }
 }
