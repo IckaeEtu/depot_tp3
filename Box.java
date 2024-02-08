f4f1f24cdd99a3987424d36dac3ec0822d3e366f
EtuIckae <mickael.riego@etu.univ-orleans.fr>
2024-02-08 10:24:52 +0100

    test modifié

--- Box.java
+++ Box.java
@@ -1,7 +1,31 @@
 import java.util.ArrayList;
+import java.lang.Throwable;
+
 class Box{
-    ArrayList<String> contents = new ArrayList<String>();
+    
+    private ArrayList<Thing> contents = new ArrayList<Thing>();
+
     public void add(String truc) {
-        this.contents.add(truc);
+        Thing thing = new Thing(truc);
+        this.contents.add(thing);
+    }
+
+    public boolean estDeDans(Thing thing){
+        return this.contents.contains(thing);
+    }
+
+    public boolean retire(String truc){
+        Thing thing = new Thing(truc);
+        try{
+            return this.contents.remove(thing);
+        }
+        catch(IndexOutOfBoundsException iobe) {
+            iobe.printStackTrace();
+            
+        }
+        finally{
+            System.out.println("finis");
+        }
+        
     }
 }
--- TestsBoxes.java
+++ TestsBoxes.java
@@ -1,5 +1,8 @@
 import org.junit.*;
 public class TestsBoxes {
+
+
+
 @Test
 public void testBoxCreate() {
 Box b = new Box();
@@ -10,5 +13,17 @@ public void testBoxAdd(){
 Box b = new Box();
 b.add("truc1");
 b.add("truc2");
+Thing thing = new Thing("truc1");
+assert b.estDeDans(thing);
+b.retire("truc2");
+}
+
+
+
+@Test(expected = IndexOutOfBoundsException.class)
+public void retireWithException() {
+        Box b = new Box();
+        b.add("truc1");
+        b.retire("truc4");
 }
 }
